ad26261bcf9b523f8a13ae1ee44ec1888dc19ab6
hito4t <thitoshi@cac.co.jp>
2015-03-07 23:23:36 +0900

    ファイル分割入力用プラグイン

---
+++ src/main/java/org/embulk/output/filesplit/LocalFileSplitInputPlugin.java
@@ -0,0 +1,176 @@
+package org.embulk.output.filesplit;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.embulk.config.CommitReport;
+import org.embulk.config.Config;
+import org.embulk.config.ConfigDiff;
+import org.embulk.config.ConfigInject;
+import org.embulk.config.ConfigSource;
+import org.embulk.config.Task;
+import org.embulk.config.TaskSource;
+import org.embulk.spi.BufferAllocator;
+import org.embulk.spi.Exec;
+import org.embulk.spi.FileInputPlugin;
+import org.embulk.spi.TransactionalFileInput;
+import org.embulk.spi.util.InputStreamFileInput;
+
+
+public class LocalFileSplitInputPlugin
+        implements FileInputPlugin
+{
+	public static class PartialFile {
+		private String path;
+		private long start;
+		private long end;
+
+		
+		public PartialFile(String path, long start, long end) {
+			this.path = path;
+			this.start = start;
+			this.end = end;
+		}
+
+		public PartialFile() {
+		}
+		
+		public String getPath() {
+			return path;
+		}
+
+
+		public void setPath(String path) {
+			this.path = path;
+		}
+
+
+		public long getStart() {
+			return start;
+		}
+
+
+		public void setStart(long start) {
+			this.start = start;
+		}
+
+
+		public long getEnd() {
+			return end;
+		}
+
+
+		public void setEnd(long end) {
+			this.end = end;
+		}
+	}
+	
+	
+	
+    public interface PluginTask
+            extends Task
+    {
+        @Config("path")
+        public String getPath();
+
+        public List<PartialFile> getFiles();
+        public void setFiles(List<PartialFile> files);
+
+        @ConfigInject
+        public BufferAllocator getBufferAllocator();
+    }
+
+    @Override
+    public ConfigDiff transaction(ConfigSource config, FileInputPlugin.Control control)
+    {
+        PluginTask task = config.loadConfig(PluginTask.class);
+
+        long size = new File(task.getPath()).length();
+        int division = Runtime.getRuntime().availableProcessors() * 2;
+        List<PartialFile> files = new ArrayList<PartialFile>();
+        for (int i = 0; i < division; i++) {
+        	long start = BigInteger.valueOf(size).multiply(BigInteger.valueOf(i)).divide(BigInteger.valueOf(division)).longValue();
+        	long end = BigInteger.valueOf(size).multiply(BigInteger.valueOf(i + 1)).divide(BigInteger.valueOf(division)).longValue();
+        	if (start < end) {
+        		files.add(new PartialFile(task.getPath(), start, end));
+        	}
+        }
+        
+        task.setFiles(files);
+
+        int taskCount = task.getFiles().size();
+        return resume(task.dump(), taskCount, control);
+    }
+
+    @Override
+    public ConfigDiff resume(TaskSource taskSource,
+            int taskCount,
+            FileInputPlugin.Control control)
+    {
+        control.run(taskSource, taskCount);
+
+        return Exec.newConfigDiff();
+    }
+
+    @Override
+    public void cleanup(TaskSource taskSource,
+            int taskCount,
+            List<CommitReport> successCommitReports)
+    { }
+
+    @Override
+    public TransactionalFileInput open(TaskSource taskSource, int taskIndex)
+    {
+        PluginTask task = taskSource.loadTask(PluginTask.class);
+        return new LocalFileSplitInput(task, taskIndex);
+    }
+
+    public static class LocalFileSplitInput
+            extends InputStreamFileInput
+            implements TransactionalFileInput
+    {
+        private static class FileSplitProvider
+                implements InputStreamFileInput.Provider
+        {
+            private final PartialFile file;
+            private boolean opened = false;
+
+            public FileSplitProvider(PartialFile file)
+            {
+                this.file = file;
+            }
+
+            @Override
+            public InputStream openNext() throws IOException
+            {
+                if (opened) {
+                    return null;
+                }
+                opened = true;
+                return new PartialFileInputStream(new FileInputStream(file.getPath()), file.getStart(), file.getEnd());
+            }
+
+            @Override
+            public void close() { }
+        }
+
+        public LocalFileSplitInput(PluginTask task, int taskIndex)
+        {
+            super(task.getBufferAllocator(), new FileSplitProvider(task.getFiles().get(taskIndex)));
+        }
+
+        @Override
+        public void abort() { }
+
+        @Override
+        public CommitReport commit()
+        {
+            return Exec.newCommitReport();
+        }
+    }
+}
---
+++ src/main/java/org/embulk/output/filesplit/PartialFileInputStream.java
@@ -0,0 +1,129 @@
+/*
+ * $Id: typical.epf 2627 2010-03-18 01:40:13Z tiba $
+ */
+package org.embulk.output.filesplit;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class PartialFileInputStream extends InputStream {
+	
+	private final InputStream original;
+	private long start;
+	private long end;
+	private long current;
+	private boolean eof;
+	
+	public PartialFileInputStream(InputStream original, long start, long end) {
+		this.original = new BufferedInputStream(original);
+		this.start = start;
+		this.end = end;
+		current = -1;
+	}
+	
+	@Override
+	public int read(byte[] b) throws IOException {
+		return read(b, 0, b.length);
+	}
+	
+	@Override
+	public int read(byte[] b, int off, int len) throws IOException {
+		initializeIfNeeded();
+
+		if (eof) {
+			return -1;
+		}
+		
+		int read = original.read(b, off, len);
+		if (read < 0) {
+			eof = true;
+			return -1;
+		}
+		
+		current += read;
+		if (current >= end) {
+			for (int i = Math.max((int)(end - current + read), 0); i < len; i++) {
+				if (b[off + i] == '\n') {
+					eof = true;
+					return i;
+				}
+			}
+		}
+		
+		return read;
+	}
+
+	@Override
+	public int read() throws IOException {
+		initializeIfNeeded();
+
+		if (eof) {
+			return -1;
+		}
+		
+		int read = original.read();
+		current++;
+		
+		if (read < 0) {
+			eof = true;
+			return -1;
+		}
+		
+		if (current >= end) {
+			if (read == '\n') {
+				eof = true;
+			}
+		}
+		return read;
+	}
+	
+	@Override
+	public long skip(long n) throws IOException {
+		throw new IOException("Skip not supported.");
+		/*
+		long skip = original.skip(n);
+		current += skip;
+		return skip;
+		*/
+	}
+	
+	@Override
+	public int available() throws IOException {
+		return 0;
+	}
+	
+	@Override
+	public void close() throws IOException {
+		original.close();
+	}
+	
+	private void initializeIfNeeded() throws IOException {
+		if (current >= start) {
+			return;
+			
+		}
+		if (start == 0) {
+			current = 0;
+		} else {
+			current = original.skip(start);
+			if (current != start) {
+				throw new IOException("Cannot skip.");
+			}
+			
+			int c;
+			while ((c = original.read()) >= 0) {
+				start++;
+				current++;
+				if (c == '\n') {
+					break;
+				}
+			}
+		}
+		
+		if (start >= end) {
+			eof = true;
+		}
+	}
+}
